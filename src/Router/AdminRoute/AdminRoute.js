0cd1cf9e00215171fab7bbaaaf88c47e4fdb9c52
Modasserul Hoque <modasserulhaq.jasim@gmail.com>
2022-11-26 07:58:28 +0600

    Admin Route added for dashboard

--- src/Router/AdminRoute/AdminRoute.js
+++ src/Router/AdminRoute/AdminRoute.js
@@ -2,18 +2,20 @@ import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import Loading from '../../components/Spinners/Loading';
 import { AuthContext } from '../../context/AuthProvider/AuthProvider';
+import useAdmin from '../../hooks/useAdmin';
 
-const PrivateRoute = ({ children }) => {
+const AdminRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
+    const [isAdmin, isAdminLoading] = useAdmin(user?.email)
     const location = useLocation();
 
-    if (loading) {
-        return <Loading></Loading>;
+    if (loading || isAdminLoading) {
+        return <Loading></Loading>
     }
-    if (user) {
+    if (user && isAdmin) {
         return children;
     }
     return <Navigate to="/login" state={{ from: location }} replace />
 };
 
-export default PrivateRoute;
+export default AdminRoute;
